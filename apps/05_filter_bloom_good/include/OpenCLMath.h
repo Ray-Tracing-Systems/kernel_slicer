94331a21e5f534b6bbee6adef41bbd030d60b43b
Vladimir Frolov <vova@frolov.pp.ru>
2020-12-26 19:11:44 +0300

    edit

---
+++ apps/05_filter_bloom_good/include/OpenCLMath.h
@@ -0,0 +1,13 @@
+#ifndef OPENCL_MATH_REPLACEMENT
+#define OPENCL_MATH_REPLACEMENT
+
+#ifndef MAXFLOAT
+#define MAXFLOAT 1e37f
+#endif
+
+typedef struct float4x4T
+{
+  float4 m_col[4];
+} float4x4;
+
+#endif
