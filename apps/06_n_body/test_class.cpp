87ab2c4be0b7f0b6c2671750a62b9c765a4dc39d
AlexandrShcherbakov <alex.shcherbakov.as@gmail.com>
2021-01-09 17:07:35 +0300

    Fixes for n body sample to generate code with kernel slicer.

--- apps/06_n_body/test_class.cpp
+++ apps/06_n_body/test_class.cpp
@@ -5,7 +5,7 @@
 using namespace LiteMath;
 
 static float randFloat(const float min_value, const float max_value) {
-  return static_cast<float>(rand()) / RAND_MAX * (max_value - min_value) + min_value;
+  return static_cast<float>(rand()) / static_cast<float>(RAND_MAX) * (max_value - min_value) + min_value;
 }
 
 static float4 randFloat4(const float4& min_value, const float4& max_value) {
@@ -19,19 +19,19 @@ static float4 randFloat4(const float4& min_value, const float4& max_value) {
 
 
 void nBody::perform() {
-  kernel1D_GenerateBodies();
+  kernel1D_GenerateBodies(BODIES_COUNT);
   for (uint32_t i = 0; i < m_iters; ++i) {
-    dumpPositions();
-    kernel1D_UpdateVelocity();
-    kernel1D_UpdatePosition();
+    // dumpPositions();
+    kernel1D_UpdateVelocity(BODIES_COUNT);
+    kernel1D_UpdatePosition(BODIES_COUNT);
   }
-  dumpPositions();
+  // dumpPositions();
 }
 
-void nBody::kernel1D_GenerateBodies() {
-  for (auto& body : m_bodies) {
-    body.pos_weight = randFloat4(float4(-1, -1, -1, -10), float4(1, 1, 1, 10));
-    body.vel_charge = randFloat4(float4(-1, -1, -1, -1), float4(1, 1, 1, 1));
+void nBody::kernel1D_GenerateBodies(uint32_t bodies_count) {
+  for (uint32_t i = 0; i < bodies_count; ++i) {
+    m_bodies[i].pos_weight = randFloat4(float4(-1, -1, -1, -10), float4(1, 1, 1, 10));
+    m_bodies[i].vel_charge = randFloat4(float4(-1, -1, -1, -1), float4(1, 1, 1, 1));
   }
 }
 
@@ -43,15 +43,19 @@ template <typename T> int sgn(T val) {
     return (T(0) < val) - (val < T(0));
 }
 
-void nBody::kernel1D_UpdateVelocity() {
-  for (uint32_t i = 0; i < m_bodies.size(); ++i) {
+static float pow3(float value) {
+  return value * value * value;
+}
+
+void nBody::kernel1D_UpdateVelocity(uint32_t bodies_count) {
+  for (uint32_t i = 0; i < bodies_count; ++i) {
     float3 acceleration(0, 0, 0);
-    for (uint32_t j = 0; j < m_bodies.size(); ++j) {
+    for (uint32_t j = 0; j < bodies_count; ++j) {
       if (i == j) {
         continue;
       }
       float3 bodyToBody = -xyz(m_bodies[j].pos_weight - m_bodies[i].pos_weight) * sgn(m_bodies[i].pos_weight.w);
-      acceleration += bodyToBody * m_bodies[j].pos_weight.w / std::pow(length(bodyToBody) + 1e-5f, 3.0f);
+      acceleration += bodyToBody * m_bodies[j].pos_weight.w / pow3(length(bodyToBody) + 1e-5f);
     }
     acceleration *= dt;
     m_bodies[i].vel_charge.x += acceleration.x;
@@ -60,19 +64,19 @@ void nBody::kernel1D_UpdateVelocity() {
   }
 }
 
-void nBody::kernel1D_UpdatePosition() {
-  for (auto& body: m_bodies) {
-    body.pos_weight.x += body.vel_charge.x * dt;
-    body.pos_weight.y += body.vel_charge.y * dt;
-    body.pos_weight.z += body.vel_charge.z * dt;
+void nBody::kernel1D_UpdatePosition(uint32_t bodies_count) {
+  for (uint32_t i = 0; i < bodies_count; ++i) {
+    m_bodies[i].pos_weight.x += m_bodies[i].vel_charge.x * dt;
+    m_bodies[i].pos_weight.y += m_bodies[i].vel_charge.y * dt;
+    m_bodies[i].pos_weight.z += m_bodies[i].vel_charge.z * dt;
   }
 }
 
-void nBody::dumpPositions() {
-  for (const auto& body: m_bodies) {
-    debugOutput.write(reinterpret_cast<const char*>(&body.pos_weight), sizeof(body.pos_weight));
-  }
-}
+// void nBody::dumpPositions() {
+//   for (const auto& body: m_bodies) {
+//     debugOutput.write(reinterpret_cast<const char*>(&body.pos_weight), sizeof(body.pos_weight));
+//   }
+// }
 
 void n_body_cpu(uint32_t seed, uint32_t iterations) {
   nBody bodies(seed, iterations);
--- apps/06_n_body/test_class.h
+++ apps/06_n_body/test_class.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <array>
+#include <vector>
 #include <fstream>
 
 class nBody
@@ -14,19 +14,20 @@ class nBody
   uint32_t m_seed;
   uint32_t m_iters;
 
-  void kernel1D_GenerateBodies();
-  void kernel1D_UpdateVelocity();
-  void kernel1D_UpdatePosition();
+  void kernel1D_GenerateBodies(uint32_t bodies_count);
+  void kernel1D_UpdateVelocity(uint32_t bodies_count);
+  void kernel1D_UpdatePosition(uint32_t bodies_count);
   ///DEBUG
-  void dumpPositions();
-  std::ofstream debugOutput;
+  // void dumpPositions();
+  // std::ofstream debugOutput;
 public:
 
   nBody(uint32_t seed, uint32_t iters) : m_seed(seed), m_iters(iters) {
-    debugOutput = std::ofstream("BodiesDumpCPU.bin", std::ios::binary);
-    debugOutput.write(reinterpret_cast<const char*>(&BODIES_COUNT), sizeof(BODIES_COUNT));
-    debugOutput.write(reinterpret_cast<const char*>(&m_iters), sizeof(m_iters));
+    // debugOutput = std::ofstream("BodiesDumpCPU.bin", std::ios::binary);
+    // debugOutput.write(reinterpret_cast<const char*>(&BODIES_COUNT), sizeof(BODIES_COUNT));
+    // debugOutput.write(reinterpret_cast<const char*>(&m_iters), sizeof(m_iters));
+    m_bodies.resize(BODIES_COUNT);
   }
   void perform();
-  std::array<BodyState, BODIES_COUNT> m_bodies;
+  std::vector<BodyState> m_bodies;
 };
