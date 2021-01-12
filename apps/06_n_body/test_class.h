95a0e3dfbe39e08ecf0728581ea0ce65237a3931
AlexandrShcherbakov <alex.shcherbakov.as@gmail.com>
2021-01-12 21:59:57 +0300

    Fix some problems with cpp generated code.

--- apps/06_n_body/test_class.h
+++ apps/06_n_body/test_class.h
@@ -7,7 +7,7 @@
 
 class nBody
 {
-  static constexpr uint32_t BODIES_COUNT = 500;
+protected:
   static constexpr float dt = 1e-3f;
   struct BodyState {
     float4 pos_weight;
@@ -23,12 +23,17 @@ class nBody
   void dumpPositions();
   std::ofstream debugOutput;
 public:
+  static constexpr uint32_t BODIES_COUNT = 500;
 
-  nBody(uint32_t seed, uint32_t iters) : m_seed(seed), m_iters(iters) {
+  nBody() {
+    m_bodies.resize(BODIES_COUNT);
+  }
+  void setParameters(uint32_t seed, uint32_t iters) {
+    m_seed = seed;
+    m_iters = iters;
     debugOutput = std::ofstream("BodiesDumpCPU.bin", std::ios::binary);
     debugOutput.write(reinterpret_cast<const char*>(&BODIES_COUNT), sizeof(BODIES_COUNT));
     debugOutput.write(reinterpret_cast<const char*>(&m_iters), sizeof(m_iters));
-    m_bodies.resize(BODIES_COUNT);
   }
   void perform();
   std::vector<BodyState> m_bodies;
