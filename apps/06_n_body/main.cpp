b9d451a30d13be02e058ac70860013bedfa49272
Vladimir Frolov <vova@frolov.pp.ru>
2021-01-18 13:42:12 +0300

    edit

--- apps/06_n_body/main.cpp
+++ apps/06_n_body/main.cpp
@@ -4,34 +4,68 @@
 std::vector<nBody::BodyState> n_body_cpu(uint32_t seed, uint32_t iterations);
 std::vector<nBody::BodyState> n_body_gpu(uint32_t seed, uint32_t iterations);
 
-bool compareFloat4(const float4& a, const float4& b) {
-  const float EPS = 1e-6f;
-  return std::abs(a.x - b.x) > EPS || std::abs(a.y - b.y) > EPS || std::abs(a.z - b.z) > EPS || std::abs(a.w - b.w) > EPS;
+const float EPS = 1e-3f;
+
+inline float errFloat4(const float4& a, const float4& b) 
+{
+  return std::max(std::max(std::abs(a.x - b.x), std::abs(a.y - b.y)), 
+                  std::max(std::abs(a.z - b.z), std::abs(a.w - b.w)));
 }
 
 int main(int argc, const char** argv)
 {
-  const uint32_t SEED = 42;
-  const uint32_t ITERATIONS = 1;
+  const uint32_t SEED       = 42;
+  const uint32_t ITERATIONS = 10;
   auto out_cpu = n_body_cpu(SEED, ITERATIONS);
   auto out_gpu = n_body_gpu(SEED, ITERATIONS);
   bool failed = false;
-  for (uint32_t i = 0; i < out_cpu.size(); ++i) {
-    if (compareFloat4(out_cpu[i].pos_weight, out_gpu[i].pos_weight)) {
-      std::cout << "Wrong position " << i << std::endl;
-      std::cout << "CPU value: " << out_cpu[i].pos_weight.x << " " << out_cpu[i].pos_weight.y << " " << out_cpu[i].pos_weight.z << " " << out_cpu[i].pos_weight.w << std::endl;
-      std::cout << "GPU value: " << out_gpu[i].pos_weight.x << " " << out_gpu[i].pos_weight.y << " " << out_gpu[i].pos_weight.z << " " << out_gpu[i].pos_weight.w << std::endl;
-      failed = true;
-      break;
+
+  std::vector<uint32_t> badId; badId.reserve(10);
+  std::vector<uint32_t> badId2; badId2.reserve(10);
+
+  float maxErr   = 0.0f;
+  float avgError = 0.0f;
+
+  for (uint32_t i = 0; i < out_cpu.size(); ++i) 
+  {
+    float errPos = errFloat4(out_cpu[i].pos_weight, out_gpu[i].pos_weight);
+    float errVel = errFloat4(out_cpu[i].vel_charge, out_gpu[i].vel_charge);
+   
+    if (errPos > EPS)
+    {
+      if(badId.size() + 1 < badId.capacity())
+        badId.push_back(i);
     }
-    if (compareFloat4(out_cpu[i].vel_charge, out_gpu[i].vel_charge)) {
-      std::cout << "Wrong velocity " << i << std::endl;
-      std::cout << "CPU value: " << out_cpu[i].vel_charge.x << " " << out_cpu[i].vel_charge.y << " " << out_cpu[i].vel_charge.z << " " << out_cpu[i].vel_charge.w << std::endl;
-      std::cout << "GPU value: " << out_gpu[i].vel_charge.x << " " << out_gpu[i].vel_charge.y << " " << out_gpu[i].vel_charge.z << " " << out_gpu[i].vel_charge.w << std::endl;
-      failed = true;
-      break;
+
+    if (errVel > EPS)
+    {
+      if(badId2.size() + 1 < badId2.capacity())
+        badId2.push_back(i);
     }
+
+    avgError += std::max(errVel,errPos);
+    maxErr = std::max(maxErr, std::max(errVel,errPos));
   }
+
+  avgError /= float(out_cpu.size());
+
+  std::cout << "maxErr = " << maxErr << std::endl;
+  std::cout << "avgErr = " << avgError << std::endl;
+
+  for(const auto i : badId)
+  {
+    std::cout << "Wrong position " << i << std::endl;
+    std::cout << "CPU value: " << out_cpu[i].pos_weight.x << "\t" << out_cpu[i].pos_weight.y << "\t" << out_cpu[i].pos_weight.z << "\t" << out_cpu[i].pos_weight.w << std::endl;
+    std::cout << "GPU value: " << out_gpu[i].pos_weight.x << "\t" << out_gpu[i].pos_weight.y << "\t" << out_gpu[i].pos_weight.z << "\t" << out_gpu[i].pos_weight.w << std::endl;
+  }
+
+  for(const auto i : badId2)
+  {
+    std::cout << "Wrong velocity " << i << std::endl;
+    std::cout << "CPU value: " << out_cpu[i].vel_charge.x << "\t" << out_cpu[i].vel_charge.y << "\t" << out_cpu[i].vel_charge.z << "\t" << out_cpu[i].vel_charge.w << std::endl;
+    std::cout << "GPU value: " << out_gpu[i].vel_charge.x << "\t" << out_gpu[i].vel_charge.y << "\t" << out_gpu[i].vel_charge.z << "\t" << out_gpu[i].vel_charge.w << std::endl;
+  }
+    
   if (failed) {
     std::cout << "FAIL" << std::endl;
   } else {
