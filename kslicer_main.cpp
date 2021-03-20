2134e5a688a5b09ce99769aacf47fe0a4cc7ba6e
Vladimir Frolov <vova@frolov.pp.ru>
2021-03-20 20:19:16 +0300

    remove folder path from build shaders command

--- kslicer_main.cpp
+++ kslicer_main.cpp
@@ -709,7 +709,7 @@ int main(int argc, const char **argv)
           currKerneJson["Kernels"].push_back(kernel.value());
 
           std::string outFileName = std::string(kernel.value()["Name"]) + ".cl";
-          std::string outFilePath = shaderPath + "/" + outFileName;
+          std::string outFilePath = inputCodeInfo.pShaderCC->ShaderFolder() + "/" + outFileName;
        
           std::ofstream file("debug.json");
           file << currKerneJson.dump(2);
