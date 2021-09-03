0f4884512bf470dfefb2a541475caaa170cf272f
Vladimir Frolov <vova@frolov.pp.ru>
2021-09-03 15:41:23 +0300

    add 07 simple rt/pt sample

---
+++ apps/07_simple_pt/include/BasicLogic.h
@@ -0,0 +1,274 @@
+#ifndef BASIC_PROJ_LOGIC_H
+#define BASIC_PROJ_LOGIC_H
+
+#define LAYOUT_STD140
+#include "LiteMath.h"
+#ifndef __OPENCL_VERSION__
+using namespace LiteMath;
+#endif
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+typedef struct Lite_HitT
+{
+  float t;
+  int   primId; 
+  int   instId;
+  int   geomId;
+} Lite_Hit;
+
+typedef struct SurfaceHitT
+{
+  float3 pos;
+  float3 norm;
+}SurfaceHit;
+
+typedef struct LightGeomT
+{
+  float3 boxMin;
+  float3 boxMax;
+} LightGeom;
+
+static inline float3 EyeRayDir(float x, float y, float w, float h, float4x4 a_mViewProjInv) // g_mViewProjInv
+{
+  float4 pos = make_float4( 2.0f * (x + 0.5f) / w - 1.0f, 
+                           -2.0f * (y + 0.5f) / h + 1.0f, 
+                            0.0f, 
+                            1.0f );
+
+  pos = mul4x4x4(a_mViewProjInv, pos);
+  pos /= pos.w;
+
+  pos.y *= (-1.0f);
+
+  return normalize(to_float3(pos));
+}
+
+static inline float2 RaySphereHit(float3 orig, float3 dir, float4 sphere) // see Ray Tracing Gems Book
+{
+  const float3 center = to_float3(sphere);
+  const float  radius = sphere.w;
+
+  // Hearn and Baker equation 10-72 for when radius^2 << distance between origin and center
+	// Also at https://www.cg.tuwien.ac.at/courses/EinfVisComp/Slides/SS16/EVC-11%20Ray-Tracing%20Slides.pdf
+	// Assumes ray direction is normalized
+	//dir = normalize(dir);
+	const float3 deltap   = center - orig;
+	const float ddp       = dot(dir, deltap);
+	const float deltapdot = dot(deltap, deltap);
+
+	// old way, "standard", though it seems to be worse than the methods above
+	//float discriminant = ddp * ddp - deltapdot + radius * radius;
+	float3 remedyTerm  = deltap - ddp * dir;
+	float discriminant = radius * radius - dot(remedyTerm, remedyTerm);
+
+  float2 result = {0,0};
+	if (discriminant >= 0.0f)
+	{
+		const float sqrtVal = sqrt(discriminant);
+		// include Press, William H., Saul A. Teukolsky, William T. Vetterling, and Brian P. Flannery, 
+		// "Numerical Recipes in C," Cambridge University Press, 1992.
+		const float q = (ddp >= 0) ? (ddp + sqrtVal) : (ddp - sqrtVal);
+		// we don't bother testing for division by zero
+		const float t1 = q;
+		const float t2 = (deltapdot - radius * radius) / q;
+    result.x = fmin(t1,t2);
+    result.y = fmax(t1,t2);
+  }
+  
+  return result;
+}
+
+static inline uint RealColorToUint32_f3(float3 real_color)
+{
+  float  r = real_color.x*255.0f;
+  float  g = real_color.y*255.0f;
+  float  b = real_color.z*255.0f;
+  unsigned char red = (unsigned char)r, green = (unsigned char)g, blue = (unsigned char)b;
+  return red | (green << 8) | (blue << 16) | 0xFF000000;
+}
+
+static inline uint RealColorToUint32(float4 real_color)
+{
+  float  r = real_color.x*255.0f;
+  float  g = real_color.y*255.0f;
+  float  b = real_color.z*255.0f;
+  float  a = real_color.w*255.0f;
+
+  unsigned char red   = (unsigned char)r;
+  unsigned char green = (unsigned char)g;
+  unsigned char blue  = (unsigned char)b;
+  unsigned char alpha = (unsigned char)a;
+
+  return red | (green << 8) | (blue << 16) | (alpha << 24);
+}
+
+#define WIN_WIDTH  512
+#define WIN_HEIGHT 512
+
+static uint pitchOffset(uint x, uint y) { return y*WIN_WIDTH + x; } 
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+static inline void CoordinateSystem(float3 v1, float3* v2, float3* v3)
+{
+  float invLen = 1.0f;
+
+  if (fabs(v1.x) > fabs(v1.y))
+  {
+    invLen = 1.0f / sqrt(v1.x*v1.x + v1.z*v1.z);
+    (*v2)  = make_float3((-1.0f) * v1.z * invLen, 0.0f, v1.x * invLen);
+  }
+  else
+  {
+    invLen = 1.0f / sqrt(v1.y * v1.y + v1.z * v1.z);
+    (*v2)  = make_float3(0.0f, v1.z * invLen, (-1.0f) * v1.y * invLen);
+  }
+
+  (*v3) = cross(v1, (*v2));
+}
+
+#ifndef M_PI
+#define M_PI          3.14159265358979323846f
+#endif
+
+#ifndef M_TWOPI
+#define M_TWOPI       6.28318530717958647692f
+#endif
+
+#ifndef INV_PI
+#define INV_PI        0.31830988618379067154f
+#endif
+
+#define GEPSILON      5e-6f
+#define DEPSILON      1e-20f
+
+//enum THREAD_FLAGS { THREAD_IS_DEAD = 2147483648};
+
+static inline float3 MapSampleToCosineDistribution(float r1, float r2, float3 direction, float3 hit_norm, float power)
+{
+  if(power >= 1e6f)
+    return direction;
+
+  const float sin_phi = sin(M_TWOPI * r1);
+  const float cos_phi = cos(M_TWOPI * r1);
+
+  //sincos(2.0f*r1*3.141592654f, &sin_phi, &cos_phi);
+
+  const float cos_theta = pow(1.0f - r2, 1.0f / (power + 1.0f));
+  const float sin_theta = sqrt(1.0f - cos_theta*cos_theta);
+
+  float3 deviation;
+  deviation.x = sin_theta*cos_phi;
+  deviation.y = sin_theta*sin_phi;
+  deviation.z = cos_theta;
+
+  float3 ny = direction, nx, nz;
+  CoordinateSystem(ny, &nx, &nz);
+
+  {
+    float3 temp = ny;
+    ny = nz;
+    nz = temp;
+  }
+
+  float3 res = nx*deviation.x + ny*deviation.y + nz*deviation.z;
+
+  float invSign = dot(direction, hit_norm) > 0.0f ? 1.0f : -1.0f;
+
+  if (invSign*dot(res, hit_norm) < 0.0f) // reflected ray is below surface #CHECK_THIS
+  {
+    res = (-1.0f)*nx*deviation.x + ny*deviation.y - nz*deviation.z;
+    //belowSurface = true;
+  }
+
+  return res;
+}
+
+static inline float epsilonOfPos(float3 hitPos) { return fmax(fmax(fabs(hitPos.x), fmax(fabs(hitPos.y), fabs(hitPos.z))), 2.0f*GEPSILON)*GEPSILON; }
+
+/**
+\brief offset reflected ray position by epsilon;
+\param  a_hitPos      - world space position on surface
+\param  a_surfaceNorm - surface normal at a_hitPos
+\param  a_sampleDir   - ray direction in which we are going to trace reflected ray
+\return offseted ray position
+*/
+static inline float3 OffsRayPos(const float3 a_hitPos, const float3 a_surfaceNorm, const float3 a_sampleDir)
+{
+  const float signOfNormal2 = dot(a_sampleDir, a_surfaceNorm) < 0.0f ? -1.0f : 1.0f;
+  const float offsetEps     = epsilonOfPos(a_hitPos);
+  return a_hitPos + signOfNormal2*offsetEps*a_surfaceNorm;
+}
+
+//static inline SurfaceHit EvalSurfaceHit(uint a_primId, float2 uv, __global uint* in_indices, __global const float4* in_vpos, __global const float4* in_vnorm)
+//{  
+//  const uint A = in_indices[a_primId*3 + 0];
+//  const uint B = in_indices[a_primId*3 + 1];
+//  const uint C = in_indices[a_primId*3 + 2];
+//  
+//  const float3 A_pos = to_float3(in_vpos[A]);
+//  const float3 B_pos = to_float3(in_vpos[B]);
+//  const float3 C_pos = to_float3(in_vpos[C]);
+//
+//  const float3 A_norm = to_float3(in_vnorm[A]);
+//  const float3 B_norm = to_float3(in_vnorm[B]);
+//  const float3 C_norm = to_float3(in_vnorm[C]);
+// 
+//  SurfaceHit hit;
+//  hit.pos  = (1.0f - uv.x - uv.y)*A_pos  + uv.y*B_pos  + uv.x*C_pos;
+//  hit.norm = (1.0f - uv.x - uv.y)*A_norm + uv.y*B_norm + uv.x*C_norm;
+//  return hit;
+//}
+
+static inline float3 EvalSurfaceNormal(float3 a_rayDir, uint a_primId, float2 uv, __global const uint* in_indices, __global const float4* in_vnorm)
+{  
+  const uint A = in_indices[a_primId*3 + 0];
+  const uint B = in_indices[a_primId*3 + 1];
+  const uint C = in_indices[a_primId*3 + 2];
+
+  const float3 A_norm = to_float3(in_vnorm[A]);
+  const float3 B_norm = to_float3(in_vnorm[B]);
+  const float3 C_norm = to_float3(in_vnorm[C]);
+ 
+  const float3 norm   = (1.0f - uv.x - uv.y)*A_norm + uv.y*B_norm + uv.x*C_norm;
+
+  const float flipNorm = dot(a_rayDir, norm) > 0.001f ? -1.0f : 1.0f;
+  return flipNorm*norm;
+}
+
+static inline float3 SphericalDirectionPBRT(const float sintheta, const float costheta, const float phi) 
+{ 
+  return make_float3(sintheta * cos(phi), sintheta * sin(phi), costheta); 
+}
+
+static inline float GGX_Distribution(const float cosThetaNH, const float alpha)
+{
+  const float alpha2  = alpha * alpha;
+  const float NH_sqr  = clamp(cosThetaNH * cosThetaNH, 0.0f, 1.0f);
+  const float den     = NH_sqr * alpha2 + (1.0f - NH_sqr);
+  return alpha2 / fmax((float)(M_PI) * den * den, 1e-6f);
+}
+
+static inline float GGX_GeomShadMask(const float cosThetaN, const float alpha)
+{
+  // Height - Correlated G.
+  //const float tanNV      = sqrt(1.0f - cosThetaN * cosThetaN) / cosThetaN;
+  //const float a          = 1.0f / (alpha * tanNV);
+  //const float lambda     = (-1.0f + sqrt(1.0f + 1.0f / (a*a))) / 2.0f;
+  //const float G          = 1.0f / (1.0f + lambda);
+
+  // Optimized and equal to the commented-out formulas on top.
+  const float cosTheta_sqr = clamp(cosThetaN*cosThetaN, 0.0f, 1.0f);
+  const float tan2         = (1.0f - cosTheta_sqr) / fmax(cosTheta_sqr, 1e-6f);
+  const float GP           = 2.0f / (1.0f + sqrt(1.0f + alpha * alpha * tan2));
+  return GP;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+#endif
---
+++ apps/07_simple_pt/test_class.cpp
@@ -0,0 +1,371 @@
+#include "test_class.h"
+#include "include/crandom.h"
+
+void TestClass::InitRandomGens(int a_maxThreads)
+{
+  m_randomGens.resize(a_maxThreads);
+  #pragma omp parallel for default(shared)
+  for(int i=0;i<a_maxThreads;i++)
+    m_randomGens[i] = RandomGenInit(i);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void TestClass::kernel_InitEyeRay(uint tid, const uint* packedXY, float4* rayPosAndNear, float4* rayDirAndFar) // (tid,tidX,tidY,tidZ) are SPECIAL PREDEFINED NAMES!!!
+{
+  const uint XY = packedXY[tid];
+
+  const uint x = (XY & 0x0000FFFF);
+  const uint y = (XY & 0xFFFF0000) >> 16;
+
+  const float3 rayDir = EyeRayDir((float)x, (float)y, (float)WIN_WIDTH, (float)WIN_HEIGHT, m_worldViewProjInv); 
+  const float3 rayPos = m_camPos;
+  
+  *rayPosAndNear = to_float4(rayPos, 0.0f);
+  *rayDirAndFar  = to_float4(rayDir, MAXFLOAT);
+}
+
+void TestClass::kernel_InitEyeRay2(uint tid, const uint* packedXY, float4* rayPosAndNear, float4* rayDirAndFar,
+                                                                   float4* accumColor,    float4* accumuThoroughput) // (tid,tidX,tidY,tidZ) are SPECIAL PREDEFINED NAMES!!!
+{
+  *accumColor        = make_float4(0,0,0,0);
+  *accumuThoroughput = make_float4(1,1,1,0);
+
+  const uint XY = packedXY[tid];
+
+  const uint x = (XY & 0x0000FFFF);
+  const uint y = (XY & 0xFFFF0000) >> 16;
+
+  const float3 rayDir = EyeRayDir((float)x, (float)y, (float)WIN_WIDTH, (float)WIN_HEIGHT, m_worldViewProjInv); 
+  const float3 rayPos = m_camPos;
+  
+  *rayPosAndNear = to_float4(rayPos, 0.0f);
+  *rayDirAndFar  = to_float4(rayDir, MAXFLOAT);
+}
+
+static float2 RayBoxIntersectionLite(const float3 ray_pos, const float3 ray_dir_inv, const float boxMin[3], const float boxMax[3])
+{
+  const float lo = ray_dir_inv.x*(boxMin[0] - ray_pos.x);
+  const float hi = ray_dir_inv.x*(boxMax[0] - ray_pos.x);
+
+  float tmin = std::min(lo, hi);
+  float tmax = std::max(lo, hi);
+
+  const float lo1 = ray_dir_inv.y*(boxMin[1] - ray_pos.y);
+  const float hi1 = ray_dir_inv.y*(boxMax[1] - ray_pos.y);
+
+  tmin = std::max(tmin, std::min(lo1, hi1));
+  tmax = std::min(tmax, std::max(lo1, hi1));
+
+  const float lo2 = ray_dir_inv.z*(boxMin[2] - ray_pos.z);
+  const float hi2 = ray_dir_inv.z*(boxMax[2] - ray_pos.z);
+
+  tmin = std::max(tmin, std::min(lo2, hi2));
+  tmax = std::min(tmax, std::max(lo2, hi2));
+
+  return make_float2(tmin, tmax); //(tmin <= tmax) && (tmax > 0.f);
+}
+
+static void IntersectAllPrimitivesInLeaf(const float4 rayPosAndNear, const float4 rayDirAndFar,
+                                         __global const uint* a_indices, uint a_start, uint a_count, __global const float4* a_vert,
+                                         Lite_Hit* pHit, float2* pBars)
+{
+  const uint triAddressEnd = a_start + a_count;
+  for (uint triAddress = a_start; triAddress < triAddressEnd; triAddress = triAddress + 3u)
+  {
+    const uint A = a_indices[triAddress + 0];
+    const uint B = a_indices[triAddress + 1];
+    const uint C = a_indices[triAddress + 2];
+
+    const float4 A_pos = a_vert[A];
+    const float4 B_pos = a_vert[B];
+    const float4 C_pos = a_vert[C];
+
+    const float4 edge1 = B_pos - A_pos;
+    const float4 edge2 = C_pos - A_pos;
+    const float4 pvec  = cross(rayDirAndFar, edge2);
+    const float4 tvec  = rayPosAndNear - A_pos;
+    const float4 qvec  = cross(tvec, edge1);
+    const float dotTmp = dot(to_float3(edge1), to_float3(pvec));
+    const float invDet = 1.0f / (dotTmp > 1e-6f ? dotTmp : 1e-6f);
+
+    const float v = dot(to_float3(tvec), to_float3(pvec))*invDet;
+    const float u = dot(to_float3(qvec), to_float3(rayDirAndFar))*invDet;
+    const float t = dot(to_float3(edge2), to_float3(qvec))*invDet;
+
+    if (v > -1e-6f && u > -1e-6f && (u + v < 1.0f + 1e-6f) && t > rayPosAndNear.w && t < pHit->t)
+    {
+      pHit->t      = t;
+      pHit->primId = triAddress/3;
+      (*pBars)     = make_float2(u,v);
+    }
+  }
+
+}
+
+static inline float3 SafeInverse_4to3(float4 d)
+{
+  const float ooeps = 1.0e-36f; // Avoid div by zero.
+  float3 res;
+  res.x = 1.0f / (fabs(d.x) > ooeps ? d.x : copysign(ooeps, d.x));
+  res.y = 1.0f / (fabs(d.y) > ooeps ? d.y : copysign(ooeps, d.y));
+  res.z = 1.0f / (fabs(d.z) > ooeps ? d.z : copysign(ooeps, d.z));
+  return res;
+}
+
+bool TestClass::kernel_RayTrace(uint tid, const float4* rayPosAndNear, float4* rayDirAndFar,
+                                Lite_Hit* out_hit, float2* out_bars)
+{
+  const float4 rayPos = *rayPosAndNear;
+  const float4 rayDir = *rayDirAndFar ;
+
+  const float3 rayDirInv = SafeInverse_4to3(rayDir);
+
+  Lite_Hit res;
+  res.primId = -1;
+  res.instId = -1;
+  res.geomId = -1;
+  res.t      = rayDir.w;
+
+  float2 baricentrics = float2(0,0);
+
+  uint nodeIdx = 0;
+  while(nodeIdx < 0xFFFFFFFE)
+  {
+    const struct BVHNode currNode = m_nodes[nodeIdx];
+    const float2 boxHit           = RayBoxIntersectionLite(to_float3(rayPos), rayDirInv, currNode.boxMin, currNode.boxMax);
+    const bool   intersects       = (boxHit.x <= boxHit.y) && (boxHit.y > rayPos.w) && (boxHit.x < res.t); // (tmin <= tmax) && (tmax > 0.f) && (tmin < curr_t)
+
+    if(intersects && currNode.leftOffset == 0xFFFFFFFF) //leaf
+    {
+      struct Interval startCount = m_intervals[nodeIdx];
+      IntersectAllPrimitivesInLeaf(rayPos, rayDir, m_indicesReordered.data(), startCount.start*3, startCount.count*3, m_vPos4f.data(), 
+                                   &res, &baricentrics);
+    }
+
+    nodeIdx = (currNode.leftOffset == 0xFFFFFFFF || !intersects) ? currNode.escapeIndex : currNode.leftOffset;
+    nodeIdx = (nodeIdx == 0) ? 0xFFFFFFFE : nodeIdx;
+  }
+  
+  // intersect flat light under roof
+  {
+    const float tLightHit  = (m_lightGeom.boxMax.y - rayPos.y)*rayDirInv.y;
+    const float4 hit_point = rayPos + tLightHit*rayDir;
+    
+    bool is_hit = (hit_point.x > m_lightGeom.boxMin.x) && (hit_point.x < m_lightGeom.boxMax.x) &&
+                  (hit_point.z > m_lightGeom.boxMin.z) && (hit_point.z < m_lightGeom.boxMax.z) &&
+                  (tLightHit < res.t);
+  
+    if(is_hit)
+    {
+      res.primId = 0;
+      res.instId = -1;
+      res.geomId = HIT_FLAT_LIGHT_GEOM;
+      res.t      = tLightHit;
+    }
+    else
+      res.geomId = HIT_TRIANGLE_GEOM;
+  }
+  
+  *out_hit  = res;
+  *out_bars = baricentrics;
+  return (res.primId != -1);
+}
+
+void TestClass::kernel_PackXY(uint tidX, uint tidY, uint* out_pakedXY)
+{
+  out_pakedXY[pitchOffset(tidX,tidY)] = ((tidY << 16) & 0xFFFF0000) | (tidX & 0x0000FFFF);
+}
+
+void TestClass::kernel_RealColorToUint32(uint tid, float4* a_accumColor, uint* out_color)
+{
+  out_color[tid] = RealColorToUint32(*a_accumColor);
+}
+
+void TestClass::kernel_GetRayColor(uint tid, const Lite_Hit* in_hit, uint* out_color)
+{
+  if(in_hit->geomId == HIT_FLAT_LIGHT_GEOM)
+  {
+    out_color[tid] = RealColorToUint32_f3(float3(1,1,1));
+  }
+  else
+  {
+    const uint32_t mtId = m_materialIds[in_hit->primId];
+    const float4 mdata  = m_materials[mtId];
+    out_color[tid]      = RealColorToUint32_f3(to_float3(mdata)); 
+  }
+}
+
+void TestClass::kernel_NextBounce(uint tid, const Lite_Hit* in_hit, const float2* in_bars, 
+                                  float4* rayPosAndNear, float4* rayDirAndFar, float4* accumColor, float4* accumThoroughput)
+{
+  // process light hit case
+  //
+  if(in_hit->geomId == HIT_FLAT_LIGHT_GEOM)
+  {
+    const float lightIntensity = m_materials[m_emissiveMaterialId].w;
+
+    if(dot(to_float3(*rayDirAndFar), float3(0,-1,0)) < 0.0f)
+      *accumColor = (*accumThoroughput)*lightIntensity;
+    else
+      *accumColor = float4(0,0,0,0);
+    return;
+  }
+  
+  // process surcase hit case
+  //
+  const Lite_Hit lHit  = *in_hit;
+  const float3 ray_dir = to_float3(*rayDirAndFar);
+  
+  SurfaceHit hit;
+  hit.pos  = to_float3(*rayPosAndNear) + lHit.t*ray_dir;
+  hit.norm = EvalSurfaceNormal(ray_dir, lHit.primId, *in_bars, m_indicesReordered.data(), m_vNorm4f.data());
+
+  RandomGen gen     = m_randomGens[tid];
+  const float2 uv   = rndFloat2_Pseudo(&gen);
+  m_randomGens[tid] = gen;
+
+  const float3 newDir   = MapSampleToCosineDistribution(uv.x, uv.y, hit.norm, hit.norm, 1.0f);
+  const float  cosTheta = dot(newDir, hit.norm);
+
+  const uint32_t mtId = m_materialIds[in_hit->primId];
+  const float4 mdata  = m_materials[mtId];
+
+  const float pdfVal   = cosTheta * INV_PI;
+  const float3 brdfVal = (cosTheta > 1e-5f) ? to_float3(mdata) * INV_PI : float3(0,0,0);
+  const float3 bxdfVal = brdfVal * (1.0f / fmax(pdfVal, 1e-10f));
+  
+  *rayPosAndNear    = to_float4(OffsRayPos(hit.pos, hit.norm, newDir), 0.0f);
+  *rayDirAndFar     = to_float4(newDir, MAXFLOAT);
+  *accumThoroughput *= cosTheta*to_float4(bxdfVal, 0.0f);
+}
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void TestClass::PackXY(uint tidX, uint tidY, uint* out_pakedXY)
+{
+  kernel_PackXY(tidX, tidY, out_pakedXY);
+}
+
+void TestClass::CastSingleRay(uint tid, uint* in_pakedXY, uint* out_color)
+{
+  float4 rayPosAndNear, rayDirAndFar;
+  kernel_InitEyeRay(tid, in_pakedXY, &rayPosAndNear, &rayDirAndFar);
+
+  Lite_Hit hit; 
+  float2   baricentrics; 
+  if(!kernel_RayTrace(tid, &rayPosAndNear, &rayDirAndFar, &hit, &baricentrics))
+    return;
+  
+  kernel_GetRayColor(tid, &hit, out_color);
+}
+
+void TestClass::kernel_ContributeToImage(uint tid, const float4* a_accumColor, const uint* in_pakedXY, float4* out_color)
+{
+  const uint XY = in_pakedXY[tid];
+  const uint x  = (XY & 0x0000FFFF);
+  const uint y  = (XY & 0xFFFF0000) >> 16;
+ 
+  out_color[y*WIN_WIDTH+x] += *a_accumColor;
+}
+
+void TestClass::NaivePathTrace(uint tid, uint a_maxDepth, uint* in_pakedXY, float4* out_color)
+{
+  float4 accumColor, accumThoroughput;
+  float4 rayPosAndNear, rayDirAndFar;
+  kernel_InitEyeRay2(tid, in_pakedXY, &rayPosAndNear, &rayDirAndFar, &accumColor, &accumThoroughput);
+
+  Lite_Hit hit; 
+  float2   baricentrics; 
+
+  for(int depth = 0; depth < a_maxDepth; depth++) 
+  {
+    Lite_Hit hit;
+    if(!kernel_RayTrace(tid, &rayPosAndNear, &rayDirAndFar, &hit, &baricentrics))
+      break;
+    
+    kernel_NextBounce(tid, &hit, &baricentrics, 
+                      &rayPosAndNear, &rayDirAndFar, &accumColor, &accumThoroughput);
+  }
+
+  kernel_ContributeToImage(tid, &accumColor, in_pakedXY, 
+                           out_color);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include "Bitmap.h"
+
+void test_class_cpu()
+{
+  TestClass test(WIN_WIDTH*WIN_HEIGHT);
+
+  std::vector<uint32_t> pixelData(WIN_WIDTH*WIN_HEIGHT);
+  std::vector<uint32_t> packedXY(WIN_WIDTH*WIN_HEIGHT);
+  std::vector<float4>   realColor(WIN_WIDTH*WIN_HEIGHT);
+  
+  // remember pitch-linear (x,y) for each thread to make our threading 1D
+  //
+  for(int y=0;y<WIN_HEIGHT;y++)
+  {
+    for(int x=0;x<WIN_WIDTH;x++)
+      test.PackXY(x, y, packedXY.data());
+  }
+
+  //test.LoadScene("lucy.bvh", "lucy.vsgf");
+  test.LoadScene("../10_virtual_func_rt_test1/cornell_collapsed.bvh", "../10_virtual_func_rt_test1/cornell_collapsed.vsgf");
+
+  // test simple ray casting
+  //
+  #pragma omp parallel for default(shared)
+  for(int i=0;i<WIN_HEIGHT*WIN_HEIGHT;i++)
+    test.CastSingleRay(i, packedXY.data(), pixelData.data());
+
+  SaveBMP("zout_cpu.bmp", pixelData.data(), WIN_WIDTH, WIN_HEIGHT);
+  
+  
+  // now test path tracing
+  //
+  const int PASS_NUMBER           = 100;
+  const int ITERS_PER_PASS_NUMBER = 4;
+  for(int passId = 0; passId < PASS_NUMBER; passId++)
+  {
+    #pragma omp parallel for default(shared)
+    for(int i=0;i<WIN_HEIGHT*WIN_HEIGHT;i++)
+    {
+      for(int j=0;j<ITERS_PER_PASS_NUMBER;j++)
+        test.NaivePathTrace(i, 6, packedXY.data(), realColor.data());
+    }
+
+    if(passId%10 == 0)
+    {
+      const float progress = 100.0f*float(passId)/float(PASS_NUMBER);
+      std::cout << "progress = " << progress << "%   \r";
+      std::cout.flush();
+    }
+  }
+  
+  //std::cout << std::endl;
+
+  const float normConst = 1.0f/float(PASS_NUMBER*ITERS_PER_PASS_NUMBER);
+  const float invGamma  = 1.0f / 2.2f;
+
+  for(int i=0;i<WIN_HEIGHT*WIN_HEIGHT;i++)
+  {
+    float4 color = realColor[i]*normConst;
+    color.x      = powf(color.x, invGamma);
+    color.y      = powf(color.y, invGamma);
+    color.z      = powf(color.z, invGamma);
+    color.w      = 1.0f;
+    pixelData[i] = RealColorToUint32(clamp(color, 0.0f, 1.0f));
+  }
+  SaveBMP("zout_cpu2.bmp", pixelData.data(), WIN_WIDTH, WIN_HEIGHT);
+
+  return;
+}
