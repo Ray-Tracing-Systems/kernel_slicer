5831bea688c843357e756c0c056aed6ef8defa74
vs <vadim.sanzharov@graphics.cs.msu.ru>
2021-09-04 12:18:35 +0300

    vkutils update

--- apps/06_n_body/test_class_gpu.cpp
+++ apps/06_n_body/test_class_gpu.cpp
@@ -4,9 +4,9 @@
 #include <chrono>
 
 #include "vk_utils.h"
-#include "vk_program.h"
+#include "vk_pipeline.h"
 #include "vk_copy.h"
-#include "vk_buffer.h"
+#include "vk_buffers.h"
 
 #include "vulkan_basics.h"
 #include "test_class_generated.h"
@@ -33,47 +33,41 @@ std::vector<nBody::BodyState> n_body_gpu(uint32_t seed, uint32_t iterations)
   enabledLayers.push_back("VK_LAYER_KHRONOS_validation");
   enabledLayers.push_back("VK_LAYER_LUNARG_standard_validation");
   VK_CHECK_RESULT(volkInitialize());
-  instance = vk_utils::CreateInstance(enableValidationLayers, enabledLayers, extensions);
+  instance = vk_utils::createInstance(enableValidationLayers, enabledLayers, extensions);
   volkLoadInstance(instance);
 
 
-  physicalDevice       = vk_utils::FindPhysicalDevice(instance, true, 0);
-  auto queueComputeFID = vk_utils::GetQueueFamilyIndex(physicalDevice, VK_QUEUE_TRANSFER_BIT | VK_QUEUE_COMPUTE_BIT);
-
+  physicalDevice       = vk_utils::findPhysicalDevice(instance, true, 0);
   // query for shaderInt8
   //
   VkPhysicalDeviceShaderFloat16Int8Features features = {};
   features.sType      = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_SHADER_FLOAT16_INT8_FEATURES;
   features.shaderInt8 = VK_TRUE;
 
-  VkPhysicalDeviceFeatures2 physDevFeatures2 = {};
-  physDevFeatures2.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_FEATURES_2;
-  physDevFeatures2.pNext = &features;
-
   std::vector<const char*> validationLayers, deviceExtensions;
   VkPhysicalDeviceFeatures enabledDeviceFeatures = {};
-  vk_utils::queueFamilyIndices fIDs = {};
+  vk_utils::QueueFID_T fIDs = {};
 
   deviceExtensions.push_back("VK_KHR_shader_non_semantic_info");
   deviceExtensions.push_back("VK_KHR_shader_float16_int8");
 
-  fIDs.compute = queueComputeFID;
-  device       = vk_utils::CreateLogicalDevice(physicalDevice, validationLayers, deviceExtensions, enabledDeviceFeatures,
-                                               fIDs, VK_QUEUE_TRANSFER_BIT | VK_QUEUE_COMPUTE_BIT, physDevFeatures2);
+  device       = vk_utils::createLogicalDevice(physicalDevice, validationLayers, deviceExtensions, enabledDeviceFeatures,
+                                               fIDs, VK_QUEUE_TRANSFER_BIT | VK_QUEUE_COMPUTE_BIT | VK_QUEUE_GRAPHICS_BIT,
+                                               &features);
   volkLoadDevice(device);
 
-  commandPool  = vk_utils::CreateCommandPool(device, physicalDevice, VK_QUEUE_COMPUTE_BIT, VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT);
+  commandPool  = vk_utils::createCommandPool(device, fIDs.compute, VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT);
 
   // (2) initialize vulkan helpers
   //  
   VkQueue computeQueue, transferQueue;
   {
-    auto queueComputeFID = vk_utils::GetQueueFamilyIndex(physicalDevice, VK_QUEUE_GRAPHICS_BIT | VK_QUEUE_COMPUTE_BIT);
-    vkGetDeviceQueue(device, queueComputeFID, 0, &computeQueue);
-    vkGetDeviceQueue(device, queueComputeFID, 0, &transferQueue);
+    vkGetDeviceQueue(device, fIDs.compute, 0, &computeQueue);
+    vkGetDeviceQueue(device, fIDs.transfer, 0, &transferQueue);
   }
 
-  auto pCopyHelper = std::make_shared<vkfw::SimpleCopyHelper>(physicalDevice, device, transferQueue, queueComputeFID, 8*1024*1024);
+  auto pCopyHelper = std::make_shared<vk_utils::SimpleCopyHelper>(physicalDevice, device, transferQueue, fIDs.transfer,
+                                                                  8*1024*1024);
 
   auto pGPUImpl = std::make_shared<nBody_Generated>();          // !!! USING GENERATED CODE !!!
   pGPUImpl->setParameters(seed, iterations);
@@ -84,14 +78,15 @@ std::vector<nBody::BodyState> n_body_gpu(uint32_t seed, uint32_t iterations)
 
   // (3) Create buffer
   //
-  VkBuffer outBuffer = vkfw::CreateBuffer(device, nBody::BODIES_COUNT * sizeof(nBody::BodyState),  VK_BUFFER_USAGE_STORAGE_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_TRANSFER_SRC_BIT);
-  VkDeviceMemory bufferMem    = vkfw::AllocateAndBindWithPadding(device, physicalDevice, {outBuffer});
+  VkBuffer outBuffer = vk_utils::createBuffer(device, nBody::BODIES_COUNT * sizeof(nBody::BodyState),
+                                              VK_BUFFER_USAGE_STORAGE_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_TRANSFER_SRC_BIT);
+  VkDeviceMemory bufferMem    = vk_utils::allocateAndBindWithPadding(device, physicalDevice, {outBuffer});
   pGPUImpl->SetVulkanInOutFor_perform(outBuffer, 0);
 
-  // now compute some thing useful
+  // now compute something useful
   //
   {
-    VkCommandBuffer commandBuffer = vk_utils::CreateCommandBuffers(device, commandPool, 1)[0];
+    VkCommandBuffer commandBuffer = vk_utils::createCommandBuffer(device, commandPool);
 
     VkCommandBufferBeginInfo beginCommandBufferInfo = {};
     beginCommandBufferInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
@@ -102,7 +97,7 @@ std::vector<nBody::BodyState> n_body_gpu(uint32_t seed, uint32_t iterations)
     vkEndCommandBuffer(commandBuffer);
 
     auto start = std::chrono::high_resolution_clock::now();
-    vk_utils::ExecuteCommandBufferNow(commandBuffer, computeQueue, device);
+    vk_utils::executeCommandBufferNow(commandBuffer, computeQueue, device);
     auto stop = std::chrono::high_resolution_clock::now();
     auto ms   = std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count()/1000.f;
     std::cout << ms << " ms for command buffer execution " << std::endl;
