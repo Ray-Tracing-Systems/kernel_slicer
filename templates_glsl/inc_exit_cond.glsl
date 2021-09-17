d5307e54d9b4c44dec57e3043c33bd42541fe296
Vladimir Frolov <vova@frolov.pp.ru>
2021-09-17 12:05:07 +0300

    fix indirect size

--- templates_glsl/inc_exit_cond.glsl
+++ templates_glsl/inc_exit_cond.glsl
@@ -1,3 +1,19 @@
+  {% if Kernel.IsIndirect %}
+  {% if Kernel.threadDim == 3 %}
+  if({{Kernel.threadName1}} >= {{Kernel.IndirectSizeX}} || {{Kernel.threadName2}} >= {{Kernel.IndirectSizeY}} || {{Kernel.threadName3}} >= {{Kernel.IndirectSizeY}})
+    return;
+  {% else if Kernel.threadDim == 2 %}
+  if({{Kernel.threadName1}} >= {{Kernel.IndirectSizeX}} || {{Kernel.threadName2}} >= {{Kernel.IndirectSizeY}})
+    return;
+  {% else %}
+  if({{Kernel.threadName1}} >= {{Kernel.IndirectSizeX}})
+    return;
+  {% endif %}
+  {% if Kernel.shouldCheckExitFlag %}
+  if((kgen_threadFlags[{{Kernel.ThreadOffset}}] & kgenArgs.tFlagsMask) != 0) 
+    return;
+  {% endif %}
+  {% else %}
   {% if Kernel.threadDim == 3 %}
   if({{Kernel.threadName1}} >= kgenArgs.{{Kernel.threadSZName1}} || {{Kernel.threadName2}} >= kgenArgs.{{Kernel.threadSZName2}} || {{Kernel.threadName3}} >= kgenArgs.{{Kernel.threadSZName3}})
     return;
@@ -12,4 +28,5 @@
   if((kgen_threadFlags[{{Kernel.ThreadOffset}}] & kgenArgs.tFlagsMask) != 0) 
     return;
   {% endif %}
+  {% endif %} {# /* if Kernel.IsIndirect */ #}
   
