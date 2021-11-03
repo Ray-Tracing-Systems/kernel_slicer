b823b7a310bfad05a29cc5203282e120eeb75052
Vladimir Frolov <vova@frolov.pp.ru>
2021-11-03 21:15:33 +0300

    edit

--- attributes.cpp
+++ attributes.cpp
@@ -27,13 +27,9 @@ namespace kslicer
 
 namespace {
 
-struct ExampleAttrInfo : public ParsedAttrInfo {
-  ExampleAttrInfo() {
-    // Can take up to 15 optional arguments, to emulate accepting a variadic
-    // number of arguments. This just illustrates how many arguments a
-    // `ParsedAttrInfo` can hold, we will not use that much in this example.
-    OptArgs = 15;
-    // [[example]] and [[plugin::example]] supported.
+struct SetterAttrInfo : public ParsedAttrInfo {
+  SetterAttrInfo() {
+    OptArgs = 1;
     static constexpr Spelling S[] = {{ParsedAttr::AS_CXX11, "kslicer::setter"}}; // {ParsedAttr::AS_CXX11, "setter"},
     Spellings = S;
   }
@@ -51,21 +47,40 @@ struct ExampleAttrInfo : public ParsedAttrInfo {
 
   AttrHandling handleDeclAttribute(Sema &S, Decl *D, const ParsedAttr &Attr) const override 
   {
-    // // Check if the decl is at file scope.
-    // if (!D->getDeclContext()->isFileContext()) 
-    // {
-    //   //unsigned ID = S.getDiagnostics().getCustomDiagID(DiagnosticsEngine::Error,"'setter' attribute only allowed at file scope");
-    //   //S.Diag(Attr.getLoc(), ID);
-    //   return AttributeNotApplied;
-    // }
-
     // Attach an annotate attribute to the Decl.
     D->addAttr(AnnotateAttr::Create(S.Context, "setter", nullptr, 0, Attr.getRange()));
-    
+    return AttributeApplied;
+  }
+};
+
+
+struct Kernel1DAttrInfo : public ParsedAttrInfo {
+  Kernel1DAttrInfo() {
+    OptArgs = 1;
+    static constexpr Spelling S[] = {{ParsedAttr::AS_CXX11, "kslicer::kernel1D"}}; // {ParsedAttr::AS_CXX11, "setter"},
+    Spellings = S;
+  }
+
+  bool diagAppertainsToDecl(Sema &S, const ParsedAttr &Attr, const Decl *D) const override 
+  {
+    // This attribute appertains to functions only.
+    if (!isa<FunctionDecl>(D)) {
+      S.Diag(Attr.getLoc(), diag::warn_attribute_wrong_decl_type_str)
+          << Attr << "functions";
+      return false;
+    }
+    return true;
+  }
+
+  AttrHandling handleDeclAttribute(Sema &S, Decl *D, const ParsedAttr &Attr) const override 
+  {
+    // Attach an annotate attribute to the Decl.
+    D->addAttr(AnnotateAttr::Create(S.Context, "kernel1D", nullptr, 0, Attr.getRange()));
     return AttributeApplied;
   }
 };
 
 } // namespace
 
-static ParsedAttrInfoRegistry::Add<ExampleAttrInfo> X("setter", "");
+static ParsedAttrInfoRegistry::Add<SetterAttrInfo> X("setter", "");
+static ParsedAttrInfoRegistry::Add<Kernel1DAttrInfo> Y("kernel1D", "");
