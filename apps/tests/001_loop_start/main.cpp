4b125e0255a510ef14e102dd91dfe2b044287aee
Vladimir Frolov <vova@frolov.pp.ru>
2021-12-11 17:48:15 +0300

    test_001

--- apps/tests/001_loop_start/main.cpp
+++ apps/tests/001_loop_start/main.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <memory>
 #include <cstdint>
+
+#include "test_class.h"
+
+#include "vk_context.h"
+std::shared_ptr<Numbers> CreateNumbers_Generated(vk_utils::VulkanContext a_ctx, size_t a_maxThreadsGenerated);
 
 int32_t array_summ_cpu(const std::vector<int32_t>& array);
 int32_t array_summ_gpu(const std::vector<int32_t>& array);
 
 int main(int argc, const char** argv)
 {
+  #ifndef NDEBUG
+  bool enableValidationLayers = true;
+  #else
+  bool enableValidationLayers = false;
+  #endif
+
   std::vector<int32_t> array(1024);
   for(size_t i=0;i<array.size();i++)
   {
@@ -17,16 +29,25 @@ int main(int argc, const char** argv)
       array[i] = -i;
   }
 
-  auto summ1 = array_summ_cpu(array);
-  auto summ2 = array_summ_gpu(array);
-
-  std::cout << "[cpu]: array summ  = " << summ1 << std::endl;
-  std::cout << "[gpu]: array summ  = " << summ2 << std::endl;
+  std::shared_ptr<Numbers> pImpl = nullptr;
+  bool onGPU = true;
   
-  if(summ1 == summ2)
-    std::cout << "test_001 PASSED!" << std::endl;
-  else 
-    std::cout << "test_001 FAILED!" << std::endl;
+  if(onGPU)
+  {
+    auto ctx = vk_utils::globalContextGet(enableValidationLayers);
+    pImpl = CreateNumbers_Generated(ctx, array.size());
+  }
+  else
+    pImpl = std::make_shared<Numbers>();
+
+  pImpl->CommitDeviceData();
+  pImpl->CalcArraySumm(array.data(), unsigned(array.size()));
+
+  if(onGPU)
+    std::cout << "[gpu]: array summ = " << pImpl->m_summ << std::endl;
+  else
+    std::cout << "[cpu]: array summ = " << pImpl->m_summ << std::endl;
+
 
   return 0;
 }
