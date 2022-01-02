5eb89e7984c770672f8c76e316cb8a271e4abe23
Vladimir Frolov <vova@frolov.pp.ru>
2022-01-02 14:50:13 +0300

    edit

--- fetch_sources.bat
+++ fetch_sources.bat
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 git submodule init 
 git submodule update
-cd apps/vkutils && git checkout slicer
+cd apps/vkutils && git checkout slicer && cd ../..
 cd apps/volk && cmake . && make
 
