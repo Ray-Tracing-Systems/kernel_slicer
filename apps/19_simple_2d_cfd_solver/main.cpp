efba9c12f09528f5687fa150750adbb9044e5406
Andrew Astapov <andrey.astapov@graphics.cs.msu.ru>
2022-01-19 23:37:38 +0300

    Sample 19: fix of zout image names

--- apps/19_simple_2d_cfd_solver/main.cpp
+++ apps/19_simple_2d_cfd_solver/main.cpp
@@ -44,10 +44,10 @@ int main(int argc, const char** argv) {
         vy[i] = randfrom(-5, 5);
 
     auto cpu_res = solve_cpu(N, density, vx, vy);
-    save_image(N,"z_out_cpu.bmp", cpu_res);
+    save_image(N,"zout_cpu.bmp", cpu_res);
 
     auto gpu_res = solve_gpu(N, density, vx, vy);
-    save_image(N, "z_out_gpu.bmp", gpu_res);
+    save_image(N, "zout_gpu.bmp", gpu_res);
 
     return 0;
 }
