1f1b044588f8676b6a3ee22fa8ea03bd8a7df136
vfrolov <vfrolov@graphics.cs.msu.ru>
2022-02-04 17:04:44 +0300

    edit

--- apps/tests/014_padding_v6/test_class.h
+++ apps/tests/014_padding_v6/test_class.h
@@ -13,10 +13,8 @@ using LiteMath::float2;
 
 struct RectLightSource
 {
-  float pos      [4];
-  float intensity[4];
-  float4 norm;
-  float size     [2];
+  float3 intensity;
+  float  value;
 };
 
 class Padding
@@ -28,14 +26,11 @@ public:
   void kernel1D_Test(float* a_data, unsigned int a_size);
   
   float4x4 matrix;
-  float test1 = 15.0f;
-  float test2 = 16.0f;
-  float test3 = 17.0f;
+  float test1 = 5.0f;
+  float test2 = 6.0f;
+  float test3 = 7.0f;
    
-  const RectLightSource m_badData = {{1.0f,2.0f,3.0f,4.0f},
-                                     {5.0f,6.0f,7.0f,8.0f},
-                                     {9.0f,10.0f,11.0f, 12.0f},
-                                     {13.0f,14.0f}};
+  const RectLightSource m_badData = {{1.0f,2.0f,3.0f}, 4.0f};
 
   virtual void CommitDeviceData() {}                                       // will be overriden in generated class
   virtual void GetExecutionTime(const char* a_funcName, float a_out[4]) {} // will be overriden in generated class
