536b46bb8b2be191ffd26016ba4284c67644eb6a
vfrolov <vfrolov@graphics.cs.msu.ru>
2022-02-07 09:35:22 +0300

    add 15 tests for vector<vec3>, vec3 aligment as vec4

---
+++ apps/tests/015_padding_v7/test_class.cpp
@@ -0,0 +1,40 @@
+#include "test_class.h"
+#include <cstdint>
+
+void Padding::Test(float* a_data, unsigned int a_size)
+{
+  kernel1D_Test(a_data, a_size);
+}
+
+void Padding::kernel1D_Test(float* a_data, unsigned int a_size)
+{
+  for(int i=0; i<a_size; i++)
+  {
+    if(i == 0)
+    {
+      a_data[0] = m_data1[0][0] + m_test.data[0];
+      a_data[1] = m_data1[0][1] + m_test.data[1];
+      a_data[2] = m_data1[0][2] + m_test.data[2];
+
+      a_data[3] = m_data1[1][0];
+      a_data[4] = m_data1[1][1];
+      a_data[5] = m_data1[1][2];
+      
+      a_data[6] = m_data1[2][0];
+      a_data[7] = m_data1[2][1];
+      a_data[8] = m_data1[2][2];
+
+      a_data[9+0] = m_data2[0].data[0];
+      a_data[9+1] = m_data2[0].data[1];
+      a_data[9+2] = m_data2[0].data[2];
+
+      a_data[9+3] = m_data2[1].data[0];
+      a_data[9+4] = m_data2[1].data[1];
+      a_data[9+5] = m_data2[1].data[2];
+      
+      a_data[9+6] = m_data2[2].data[0];
+      a_data[9+7] = m_data2[2].data[1];
+      a_data[9+8] = m_data2[2].data[2];
+    }
+  }
+}
---
+++ apps/tests/015_padding_v7/test_class.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <vector>
+#include <iostream>
+#include <fstream>
+
+#define LAYOUT_STD140
+#include "LiteMath.h"
+
+using LiteMath::float4x4;
+using LiteMath::float4;
+using LiteMath::float3;
+using LiteMath::float2;
+
+struct MyFloat3 { float data[3]; };
+
+class Padding
+{
+public:
+  Padding()
+  { 
+    m_data1 = {{1.0f,2.0f,3.0f}, {4.0f,5.0f,6.0f}, {7.0f,8.0f,9.0f}};
+    m_data2 = {{-1.0f,-2.0f,-3.0f}, {-4.0f,-5.0f,-6.0f}, {-7.0f,-8.0f,-9.0f}}; 
+  }
+
+  virtual void Test(float* a_data __attribute__((size("a_size"))), unsigned int a_size);
+  void kernel1D_Test(float* a_data, unsigned int a_size);
+
+  std::vector<float3>   m_data1;
+  std::vector<MyFloat3> m_data2;
+
+  const MyFloat3 m_test = {{0.0f, 0.0f, 0.0f}};
+
+  virtual void CommitDeviceData() {}                                       // will be overriden in generated class
+  virtual void GetExecutionTime(const char* a_funcName, float a_out[4]) {} // will be overriden in generated class
+};
