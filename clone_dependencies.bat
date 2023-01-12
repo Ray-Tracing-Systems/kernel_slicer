7dad194b00aad4794f22ba7a851fd54f0ef0551e
Vladimir Frolov <vova@frolov.pp.ru>
2023-01-12 13:53:42 +0300

    edit

--- clone_dependencies.bat
+++ clone_dependencies.bat
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
-git clone --depth 1 https://github.com/msu-graphics-group/LiteMath.git
+cd apps
 git clone --depth 1 https://github.com/zeux/volk.git
-cd apps/volk && cmake . && make && cd ../..
+git clone --depth 1 https://github.com/msu-graphics-group/vk-utils.git vkutils
+cd volk && cmake . && make && cd ../..
 
