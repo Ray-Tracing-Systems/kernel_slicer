dedb316f3bb500659c88478efa7e0be4f12fbd16
Vladimir Frolov <vova@frolov.pp.ru>
2023-06-16 15:21:03 +0300

    edit

--- templates_glsl/common_generated.h
+++ templates_glsl/common_generated.h
@@ -33,6 +33,7 @@
 #define FLT_MAX 1e37f
 #define FLT_MIN -1e37f
 #define FLT_EPSILON 1e-6f
+#define DEG_TO_RAD  0.017453293f
 #define unmasked
 #define half  float16_t
 #define half2 f16vec2
@@ -51,6 +52,39 @@
 /////////////////////////////////////////////////////////////////////
 bool  isfinite(float x)            { return !isinf(x); }
 float copysign(float mag, float s) { return abs(mag)*sign(s); }
+
+mat4 translate4x4(vec3 delta)
+{
+  return mat4(vec4(1.0, 0.0, 0.0, 0.0),
+              vec4(0.0, 1.0, 0.0, 0.0),
+              vec4(0.0, 0.0, 1.0, 0.0),
+              vec4(delta, 1.0));
+}
+
+mat4 rotate4x4X(float phi)
+{
+  return mat4(vec4(1.0f, 0.0f,  0.0f,           0.0f),
+              vec4(0.0f, +cos(phi),  +sin(phi), 0.0f),
+              vec4(0.0f, -sin(phi),  +cos(phi), 0.0f),
+              vec4(0.0f, 0.0f,       0.0f,      1.0f));
+}
+
+mat4 rotate4x4Y(float phi)
+{
+  return mat4(vec4(+cos(phi), 0.0f, -sin(phi), 0.0f),
+              vec4(0.0f,      1.0f, 0.0f,      0.0f),
+              vec4(+sin(phi), 0.0f, +cos(phi), 0.0f),
+              vec4(0.0f,      0.0f, 0.0f,      1.0f));
+}
+
+mat4 rotate4x4Z(float phi)
+{
+  return mat4(vec4(+cos(phi), sin(phi), 0.0f, 0.0f),
+              vec4(-sin(phi), cos(phi), 0.0f, 0.0f),
+              vec4(0.0f,      0.0f,     1.0f, 0.0f),
+              vec4(0.0f,      0.0f,     0.0f, 1.0f));
+}
+
 
 ## for LocalFunc in LocalFunctions  
 {{LocalFunc}}
