b5e28f85462760e66dc6b1e84657ca75dc6de6e2
vf_amdgpu_laptop <vova@frolov.pp.ru>
2024-04-29 19:04:28 +0300

    edit

--- apps/tests/030_block_expansion_v1/main.cpp
+++ apps/tests/030_block_expansion_v1/main.cpp
@@ -9,8 +9,8 @@
 #define JSON_LOG_IMPLEMENTATION
 #include "JSONLog.hpp"
 
-//#include "vk_context.h"
-//std::shared_ptr<TestClass> CreateTestClass_Generated(vk_utils::VulkanContext a_ctx, size_t a_maxThreadsGenerated);
+#include "vk_context.h"
+std::shared_ptr<TestClass> CreateTestClass_Generated(vk_utils::VulkanContext a_ctx, size_t a_maxThreadsGenerated);
 
 int main(int argc, const char** argv)
 {
@@ -27,13 +27,13 @@ int main(int argc, const char** argv)
 
   bool onGPU = args.hasOption("--gpu");
   
-  //if(onGPU)
-  //{
-  //  unsigned int a_preferredDeviceId = args.getOptionValue<int>("--gpu_id", 0);
-  //  auto ctx = vk_utils::globalContextGet(enableValidationLayers, a_preferredDeviceId);
-  //  pImpl = CreateTestClass_Generated(ctx, array.size());
-  //}
-  //else
+  if(onGPU)
+  {
+    unsigned int a_preferredDeviceId = args.getOptionValue<int>("--gpu_id", 0);
+    auto ctx = vk_utils::globalContextGet(enableValidationLayers, a_preferredDeviceId);
+    pImpl = CreateTestClass_Generated(ctx, array.size());
+  }
+  else
     pImpl = std::make_shared<TestClass>();
 
   std::string backendName = onGPU ? "gpu" : "cpu";
@@ -42,7 +42,7 @@ int main(int argc, const char** argv)
   pImpl->Test(unsigned(array.size()), array.data());
 
   for(int i=0;i<256;i++)
-    std::cout << i << "\t" << array[i] << std::endl;
+    std::cout << i << ", " << array[i] << std::endl;
   
   //uint32_t outArray[1024] = {};
   //float    outArray2[1024] = {};
