78edbb657f29a29e2401105ab572c9ae7b42e129
Vladimir Frolov (vsan old mashine) <vova@frolov.pp.ru>
2024-06-14 14:31:46 +0300

    inheritance sample

---
+++ apps/tests/034_inheritance_v1/test_class.cpp
@@ -0,0 +1,32 @@
+#include "test_class.h"
+#include <cstdint>
+
+void Base::Init(size_t a_size)
+{
+  dataInBaseClass = 2.0f;
+  vInBase.resize(a_size);
+  for(size_t i=0;i<a_size;i++)
+    vInBase[i] = i*2.0f;
+}
+
+void Derived::Init(size_t a_size)
+{
+  Base::Init(a_size);
+  dataInDerivedClass = 2.0f;
+  vInDerived.resize(a_size);
+  for(size_t i=0;i<a_size;i++)
+    vInDerived[i] = i*2.0f;
+}
+
+void Derived::Test(float* a_data, unsigned int a_size)
+{
+  kernel1D_Test(a_data, a_size);
+}
+
+void Derived::kernel1D_Test(float* a_data, unsigned int a_size)
+{
+  for(int i=0; i<a_size; i++)
+  {
+    a_data[i] = dataInBaseClass*vInBase[i] + dataInDerivedClass*vInDerived[i];
+  }
+}
