1b67ce30e11513de3c318571a8a890b9598cc422
vova-msu-cmc <vladimir.frolov@graphics.cs.msu.ru>
2024-08-07 10:32:44 +0300

    fix shader build script

--- apps/tests/035_intersection_shader/shaders_generated/build.sh
+++ apps/tests/035_intersection_shader/shaders_generated/build.sh
@@ -3,7 +3,6 @@ glslangValidator -V --target-env vulkan1.2 -S rgen BFRT_ReadAndComputeMegaRGEN.g
 
 glslangValidator -V --target-env vulkan1.2 -S rchit z_trace_rchit.glsl -o z_trace_rchit.glsl.spv
 glslangValidator -V --target-env vulkan1.2 -S rmiss z_trace_rmiss.glsl -o z_trace_rmiss.glsl.spv
-glslangValidator -V --target-env vulkan1.2 -S rmiss z_trace_smiss.glsl -o z_trace_smiss.glsl.spv
 
 glslangValidator -V --target-env vulkan1.2 -S rchit z_SpherePrim_rchit.glsl -o z_SpherePrim_rchit.glsl.spv
 glslangValidator -V --target-env vulkan1.2 -S rint  z_SpherePrim_rcint.glsl -o z_SpherePrim_rcint.glsl.spv
