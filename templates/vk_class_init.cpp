c5f15dc931dfe26ee6f5016c9764201ae961dc90
Vladimir Frolov (vsan old mashine) <vova@frolov.pp.ru>
2024-08-17 12:09:41 +0300

    edit

--- templates/vk_class_init.cpp
+++ templates/vk_class_init.cpp
@@ -1389,7 +1389,7 @@ void {{MainClassName}}{{MainClassSuffix}}::AllocAllShaderBindingTables(const std
 
   m_allShaderTableBuffers.clear();
 
-  uint32_t numShaderGroups = 2 + {{length(IntersectionHierarhcy.Implementations)}}; // (raygen, miss) + ({% for Impl in IntersectionHierarhcy.Implementations %}{{Impl.ClassName}}, {% endfor %})
+  uint32_t numShaderGroups = 3 + {{length(IntersectionHierarhcy.Implementations)}}; // (raygen, miss, rchit(tris)) + ({% for Impl in IntersectionHierarhcy.Implementations %}{{Impl.ClassName}}, {% endfor %})
   uint32_t numHitStages    = std::max(uint32_t(sbtRecordOffsets.size()), 1u); // 1 if we don't have actual sbtRecordOffsets at all
   uint32_t numMissStages   = 1u; 
 
