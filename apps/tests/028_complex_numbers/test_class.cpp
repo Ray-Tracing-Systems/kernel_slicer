15c4c0b59583621d99fc5267024698fbcbf27a5f
vova-msu-cmc <vladimir.frolov@graphics.cs.msu.ru>
2024-10-23 13:09:09 +0300

    edit

--- apps/tests/028_complex_numbers/test_class.cpp
+++ apps/tests/028_complex_numbers/test_class.cpp
@@ -9,6 +9,23 @@ static inline complex filmPhaseDiff(complex cosTheta, complex eta, float thickne
   return 4 * M_PI * eta * cosTheta * thickness / lambda;
 }
 
+static inline complex FrComplexRefl(complex cosThetaI, complex cosThetaT, complex iorI, complex iorT, int polarization)
+{
+  if (complex_norm(cosThetaI) < 1e-6f) //approximated
+  {
+    return {-1, 0};
+  }
+  if (polarization == 1)
+  {
+    return (iorI * cosThetaI - iorT * cosThetaT) / (iorI * cosThetaI + iorT * cosThetaT);
+  }
+  else if (polarization == 2)
+  {
+    return (iorT * cosThetaI - iorI * cosThetaT) / (iorT * cosThetaI + iorI * cosThetaT);
+  }
+  return 1.0f;
+}
+
 void TestClass::Test(BoxHit* a_data, unsigned int a_size)
 {
   Cow testCow;
@@ -22,7 +39,8 @@ void TestClass::kernel1D_Test(BoxHit* a_data, uint32_t a_size, Cow a_cow)
   {
     complex a = complex(1.0f, 0.0f);
     complex b = complex(1.0f, -1.0f);
-    complex test = filmPhaseDiff(a, b, 3.0f, 4.0f);
+    complex test  = filmPhaseDiff(a, b, 3.0f, 4.0f);
+    complex test2 = FrComplexRefl(a, b, 1.5f, 1.0f, 1);
     a_data[i] = make_BoxHit(i, a_cow.moooo + test.re + test.im);
   }
 }
