aac9dc220899d1504320efada967cef3d871abca
Vladimir Frolov <vova@frolov.pp.ru>
2025-02-10 20:26:28 +0300

    fix ISPC

--- kslicer.h
+++ kslicer.h
@@ -1215,6 +1215,7 @@ namespace kslicer
     std::string BuildCommand(const std::string& a_inputFile) const override;
     std::string PrintHeaderDecl(const DeclInClass& a_decl, const clang::CompilerInstance& a_compiler, std::shared_ptr<kslicer::FunctionRewriter> a_pRewriter) override;
     std::string ReplaceCallFromStdNamespace(const std::string& a_call, const std::string& a_typeName) const override;
+    bool        BuffersAsPointersInShaders() const override { return false; }
   };
 
   struct GLSLCompiler : IShaderCompiler
