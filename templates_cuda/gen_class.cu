19ffebb2eb042fad388999fa8dd17521af999a0e
vova-msu-cmc <vladimir.frolov@graphics.cs.msu.ru>
2025-03-24 15:47:07 +0300

    edit

--- templates_cuda/gen_class.cu
+++ templates_cuda/gen_class.cu
@@ -20,16 +20,16 @@ namespace {{MainClassName}}{{MainClassSuffix}}_DEV
   {% for Kernel in KernelList %}
   __device__ void {{Kernel.Name}}({%for Arg in Kernel.OriginalArgs %}{{Arg.Type}} {{Arg.Name}}{% if loop.index != Kernel.LastArgAll %}, {% endif %}{% endfor %})
   {
-    int _threadIndices[3] = {
+    const uint _threadID[3] = {
       blockIdx.x * blockDim.x + threadIdx.x,
       blockIdx.y * blockDim.y + threadIdx.y,
       blockIdx.z * blockDim.z + threadIdx.z
     };
     {% for TID in Kernel.ThreadIds %}
     {% if TID.Simple %}
-    const {{TID.Type}} {{TID.Name}} = {{TID.Type}}(_threadIndices[{{ loop.index }}]); 
+    const {{TID.Type}} {{TID.Name}} = {{TID.Type}}(_threadID[{{ loop.index }}]); 
     {% else %}
-    const {{TID.Type}} {{TID.Name}} = {{TID.Start}} + {{TID.Type}}(_threadIndices[{{ loop.index }}])*{{TID.Stride}}; 
+    const {{TID.Type}} {{TID.Name}} = {{TID.Start}} + {{TID.Type}}(_threadID[{{ loop.index }}])*{{TID.Stride}}; 
     {% endif %}
     {% endfor %}
     {{Kernel.Source}}
@@ -199,7 +199,7 @@ void {{MainClassName}}{{MainClassSuffix}}::{{Kernel.OriginalDecl}}
 {
   dim3 block({{Kernel.WGSizeX}}, {{Kernel.WGSizeY}}, {{Kernel.WGSizeZ}});
   dim3 grid(({{Kernel.tidX}} + block.x - 1) / block.x, ({{Kernel.tidY}} + block.y - 1) / block.y, ({{Kernel.tidZ}} + block.z - 1) / block.z);
-  {{MainClassName}}{{MainClassSuffix}}_DEV::{{Kernel.Name}}<<<grid, block>>>({%for Arg in Kernel.OriginalArgs %}{{Arg.Name}}{% if loop.index != Kernel.LastArgAll %}, {% endif %}{% endfor %});
+  {{MainClassName}}{{MainClassSuffix}}_DEV::{{Kernel.OriginalName}}<<<grid, block>>>({%for Arg in Kernel.OriginalArgs %}{{Arg.Name}}{% if loop.index != Kernel.LastArgAll %}, {% endif %}{% endfor %});
 }
 
 {% endfor %}
@@ -230,7 +230,7 @@ void {{MainClassName}}{{MainClassSuffix}}::{{Kernel.OriginalDecl}}
   cudaMemcpy({{var.Name}}Host, {{var.Name}}, {{var.DataSize}}*sizeof({{var.DataType}}), cudaMemcpyDeviceToHost);
   {% endfor %}
   {% for var in MainFunc.FullImpl.InputData %}
-  cudaFree({{var.Name}});
+  cudaFree((void*){{var.Name}});
   {% endfor %}
   {% for var in MainFunc.FullImpl.OutputData %}
   cudaFree({{var.Name}});
