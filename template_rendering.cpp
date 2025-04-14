bcb64523e22ae16d5000668588d74e495c042535
vova-msu-cmc <vladimir.frolov@graphics.cs.msu.ru>
2025-04-14 15:56:45 +0300

    restrict

--- template_rendering.cpp
+++ template_rendering.cpp
@@ -124,7 +124,7 @@ nlohmann::json kslicer::GetOriginalKernelJson(const KernelInfo& k, const MainCla
     std::string typeName = pShaderRewriter->RewriteStdVectorTypeStr(arg.type);
     json argj;
     if(a_classInfo.pShaderCC->IsCUDA() && arg.IsPointer()) // strange bug with spaces when use template text rendering for that
-      typeName += "*";                                     // 
+      typeName += "* __restrict__ ";                        // 
     argj["Type"]      = typeName;
     argj["Name"]      = arg.name;
     argj["IsPointer"] = arg.IsPointer();
