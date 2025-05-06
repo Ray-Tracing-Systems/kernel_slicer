2645f01211c2e6efa10f0b5d25d2e0e6b4a89773
vova-msu-cmc <vladimir.frolov@graphics.cs.msu.ru>
2025-05-06 13:47:46 +0300

    in progress with 'BaseClass::Func' calls

--- shaders_glsl.cpp
+++ shaders_glsl.cpp
@@ -814,9 +814,13 @@ std::string kslicer::GLSLFunctionRewriter::RewriteFuncDecl(clang::FunctionDecl* 
     int a = 2;
   }
 
-  if(m_pCurrFuncInfo != nullptr && m_pCurrFuncInfo->hasPrefix) // alter function name if it has any prefix
+  if(m_pCurrFuncInfo != nullptr && m_pCurrFuncInfo->hasPrefix)          // alter function name if it has any prefix
+  { 
     if(fname.find(m_pCurrFuncInfo->prefixName) == std::string::npos)
       fname = m_pCurrFuncInfo->prefixName + "_" + fname;
+  }
+  else if(m_pCurrFuncInfo != nullptr && m_pCurrFuncInfo->name != fname) // alter function name if was changed
+    fname = m_pCurrFuncInfo->name;
 
   std::string result = retT + " " + fname + "(";
 
