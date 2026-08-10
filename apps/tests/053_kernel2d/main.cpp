71af7dfa524c44758943e1786b9e4fed0d78d92d
vova_cmc <vladimir.frolov@graphics.cs.msu.ru>
2026-08-10 14:06:33 +0300

    add 53 test for 2D kernels

---
+++ apps/tests/053_kernel2d/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cstdint>
+#include <cassert>
+#include <memory>
+
+#include "test_class.h"
+
+#include "ArgParser.h"
+#define JSON_LOG_IMPLEMENTATION
+#include "JSONLog.hpp"
+
+#ifdef USE_VULKAN
+#include "vk_context.h"
+std::shared_ptr<Test2D> CreateTest2D_Generated(size_t a_size, vk_utils::VulkanContext a_ctx, size_t a_maxThreadsGenerated);
+vk_utils::VulkanDeviceFeatures Test2D_Generated_ListRequiredDeviceFeatures();
+#endif
+
+int main(int argc, const char** argv)
+{
+  #ifndef NDEBUG
+  bool enableValidationLayers = true;
+  #else
+  bool enableValidationLayers = false;
+  #endif
+  
+  const size_t size = 256;
+  std::vector<int> color(size);
+ 
+  ArgParser args(argc, argv);
+  bool onGPU = args.hasOption("--gpu"); 
+
+  std::shared_ptr<Test2D> pImpl = nullptr;
+  #ifdef USE_VULKAN
+  if(onGPU)
+  {
+    unsigned int a_preferredDeviceId = args.getOptionValue<int>("--gpu_id", 0);
+    auto features = Test2D_Generated_ListRequiredDeviceFeatures();
+    auto ctx      = vk_utils::globalContextInit(features, enableValidationLayers, a_preferredDeviceId);
+    pImpl = CreateTest2D_Generated(size, ctx, size);
+  }
+  else
+  #endif
+    pImpl = std::make_shared<Test2D>(size);
+
+  pImpl->CommitDeviceData();
+
+  pImpl->Run(size, color.data());
+  
+  std::string backendName = onGPU ? "gpu" : "cpu";
+  JSONLog::write("array", color);
+  JSONLog::saveToFile("zout_"+backendName+".json");
+
+  pImpl = nullptr;
+  #ifdef USE_VULKAN
+  vk_utils::globalContextDestroy();
+  #endif
+  return 0;
+}
